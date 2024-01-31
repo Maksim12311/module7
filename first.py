2e64c0da3a0867ffcfceb74ad3330ec88ecfc93b
Maksim12311 <max.brus.2016@gmail.com>
2024-01-31 17:59:40 +0300

    Its first task

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("Module7")
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
+page = requests.get('https://mfd.ru/currency/?currency=USD')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+currencys = soup.find_all('table', {'class':'mfd-currency-table'})
+
+
+print([currency.text for currency in currencys])
